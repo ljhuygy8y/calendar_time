f7d5d6b8baa01bd2e2a40bdb35b0e7bae99e3f38
CnAPTAK09 <reus.21012009@gmail.com>
2024-12-18 18:21:07 +0200

    update js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let year = document.querySelector(".year");
+let mounth = document.querySelector(".mounth");
+let day = document.querySelector(".day");
+let dayofweek = document.querySelector(".dayofweek");
+
+let today = new Date();
+console.log(today);
+
+let mounths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let daus = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+year.innerHTML = today.getFullYear();
+mounth.innerHTML = mounths[today.getMonth()];
+day.innerHTML = today.getDate();
+dayofweek.innerHTML = days[today.getDay()];
+
+
 
